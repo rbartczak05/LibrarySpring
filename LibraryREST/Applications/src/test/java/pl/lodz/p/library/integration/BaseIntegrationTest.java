9a2f64d7329cb51dd573f6901b493008ca89e426
Jan Kozłowski <janek1410.jk@gmail.com>
2026-03-24 23:23:43 +0100

    Dodanie DependencyManagment i pluginManagment do głównego pom.xml

--- LibraryREST/Applications/src/test/java/pl/lodz/p/library/integration/BaseIntegrationTest.java
+++ LibraryREST/Applications/src/test/java/pl/lodz/p/library/integration/BaseIntegrationTest.java
@@ -2,22 +2,22 @@ package pl.lodz.p.library.integration;
 
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.http.ContentType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import pl.lodz.p.library.adapters.rest.security.JwtService;
 import pl.lodz.p.library.domain.model.Administrator;
 import pl.lodz.p.library.ports.inbound.UserUseCase;
 import pl.lodz.p.library.ports.outbound.*;
-
-import static io.restassured.RestAssured.given;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
@@ -44,6 +44,9 @@ public abstract class BaseIntegrationTest {
     @Autowired protected UserUseCase userUseCase;
     @Autowired protected PasswordEncoder passwordEncoder;
 
+    @Autowired protected JwtService jwtService;
+    @Autowired protected UserDetailsService userDetailsService;
+
     @DynamicPropertySource
     static void setProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
@@ -53,7 +56,6 @@ public abstract class BaseIntegrationTest {
     void setUp() {
         RestAssured.port = port;
         RestAssured.baseURI = "http://localhost";
-
         RestAssured.authentication = RestAssured.DEFAULT_AUTH;
 
         getLoanPort.findAll().forEach(loan -> deleteLoanPort.deleteLoan(loan.getId()));
@@ -64,15 +66,8 @@ public abstract class BaseIntegrationTest {
         userUseCase.changeUserPasswordInModel(admin, passwordEncoder.encode("admin123"));
         saveUserPort.addUser(admin);
 
-        String token = given()
-                .contentType(ContentType.JSON)
-                .body("{\"login\":\"admin_test\",\"password\":\"admin123\"}")
-                .when()
-                .post("/auth/login")
-                .then()
-                .statusCode(200)
-                .extract()
-                .path("token");
+        UserDetails userDetails = userDetailsService.loadUserByUsername("admin_test");
+        String token = jwtService.generateAccessToken(userDetails);
 
         RestAssured.requestSpecification = new RequestSpecBuilder()
                 .addHeader("Authorization", "Bearer " + token)
