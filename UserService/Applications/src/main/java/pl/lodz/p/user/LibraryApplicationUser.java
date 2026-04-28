a1568759ab90edcdb413446a6b79669469a9160d
rbartczak05 <bartczakremigiusz@gmail.com>
2026-04-28 13:58:58 +0200

    Naprawa uruchamiania aplikacji

--- UserService/Applications/src/main/java/pl/lodz/p/user/LibraryApplicationUser.java
+++ UserService/Applications/src/main/java/pl/lodz/p/user/LibraryApplicationUser.java
@@ -12,9 +12,9 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
         "pl.lodz.p.user.services"
 })
 @EnableMongoRepositories(basePackages = "pl.lodz.p.user.adapters.mongo.repositories")
-public class LibraryApplicationUsr {
+public class LibraryApplicationUser {
 
     static void main(String[] args) {
-        SpringApplication.run(LibraryApplicationUsr.class, args);
+        SpringApplication.run(LibraryApplicationUser.class, args);
     }
 }
